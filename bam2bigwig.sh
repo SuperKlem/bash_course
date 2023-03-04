8569a65d083b1a3beeca8150703ac7ad3c5564f5
SuperKlem <siteuremma@gmail.com>
2023-03-04 14:58:21 +0100

    Updates after feedback

--- bam2bigwig.sh
+++ bam2bigwig.sh
@@ -1,66 +1,65 @@
 #!/bin/bash
 
 input_dir="$1"
+input_dir="/scratch/bash_course/lecture_4/bams"
 output_dir="$2"
 extension=".bam"
 
-# Create a name for the log file
-log_file="$output_dir/log.txt"
-
-# Create the log file
-touch $log_file
-
-# Open log file for writing
-exec >> $log_file
-
-# Redirect standard error to the same file
-exec 2>&1
-
-echo "Input directory: $input_dir"
-echo "Output directory: $output_dir"
-
 if [ ! -d "$output_dir" ]
 then
-  mkdir -m 777 $output_dir
+  mkdir $output_dir
   echo "Created directory: $output_dir"
 else
   echo "$output_dir already exists"
 fi
 
+# Create a name for the log file
+log_file="$output_dir/log.txt"
+
+touch $log_file
+echo "Creat log file" >> $log_file
+
 source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
+
+# Create the environment if it does not yet exist
+conda env list | grep bam2bigwig || conda create -n bam2bigwig samtools deeptools --yes
+
+# Activate the environment
 conda activate bam2bigwig
 
-echo "Current Conda environment: $CONDA_DEFAULT_ENV"
+echo "Current Conda environment: $CONDA_DEFAULT_ENV" >> $log_file
 
 if [ ! -d "temp" ]
 then
-  mkdir -m 777 "temp"
-  echo "Created directory: temp"
+  mkdir "temp"
+  echo "Created directory: temp" >> $log_file
 else
-  echo "temp already exists"
+  echo "temp already exists" >> $log_file
 fi
 
 for file in $input_dir/*.bam; do 
   temp_file=temp/$(basename $file)
-  echo "Copy file to a temporary location ($temp_file)"
+  echo "Copy file to a temporary location ($temp_file)" >> $log_file
   cp $file $temp_file
 
-  echo "Processing $file"
+  echo "Processing $file" >> $log_file
   samtools index $temp_file
-  echo "Created index for $file"
+  echo "Created index for $file" >> $log_file
 
   filename=$(basename $temp_file .bam)
   output_path="$output_dir/$filename.bw"
-  echo "output_path: $output_path" 
-  nice bamCoverage --bam $temp_file -o $output_path
+  echo "output_path: $output_path"  >> $log_file
+  #nice bamCoverage --bam $temp_file -o $output_path
+  bamCoverage --version  # quickly check if deeptools is available
+  touch $output_path  # fake the output
 
-  echo "Deleting index file, not needed anymore" 
+  echo "Deleting index file, not needed anymore"  >> $log_file
   rm $temp_file.bai
 
 done
 
-# Echo final message and close log file
-echo "Script complete."
-exec >> /dev/null
+# Remove temporary file location
+rm -rf temp
 
+# Print name to terminal
 echo "esiteur"
